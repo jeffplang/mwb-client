cd1f5ce8fee137483a8317abc708b32b7acb548e
Jeff Lang <jeffplang@gmail.com>
2022-06-23 22:52:33 -0700

    basic code and error handling for fetching and displaying results

--- mwb_client.rb
+++ mwb_client.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'net/http'
+require 'json'
+require 'csv'
 
 options = {
   format: :json
@@ -28,4 +31,39 @@ optsparser.parse!
 if ARGV.empty?
   puts optsparser
   exit
+end
+
+body = {
+  'query' => 'get_taginfo',
+  'tag' => ARGV[0],
+  'limit' => 50
+}
+
+fields = ["first_seen", "last_seen", "file_type", "md5_hash", "sha256_hash", "tags", "reporter"]
+
+uri = URI('https://mb-api.abuse.ch/api/v1/')
+res = Net::HTTP.post_form(uri, body)
+
+if res.code != "200"
+  puts "HTTP Error: #{res.code}"
+  puts res.body
+  exit(1)
+end
+
+parsed = JSON.parse(res.body)
+
+unless ["ok", "no_results"].include? parsed["query_status"]
+  puts "API Error: #{parsed["query_status"]}"
+  exit(1)
+end
+
+data = (parsed['data'] || []).map do |row|
+  row["tags"] = row["tags"].join(', ')
+  row.slice(*fields)
+end
+
+if options[:format] == :csv
+  puts data.map(&:values).map(&:to_csv)
+else
+  puts data.to_json
 end
